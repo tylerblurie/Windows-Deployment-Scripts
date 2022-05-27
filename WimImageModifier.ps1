55d639586aa5c9a5ddb9a0e9568f44dbf61f6868
tylerblurie <96397157+tylerblurie@users.noreply.github.com>
2022-05-26 22:48:11 -0400

    Script now mounts ISOs and sanitizes user input

--- WimImageModifier.ps1
+++ WimImageModifier.ps1
@@ -1,25 +1,49 @@
-$QUIT = "8"
+$QUIT = 10 # Used to exit the program
+$mountDrive = "" # Drive storing the ISO file the user is modifying
 
 function Print-Menu() {
-    Write-Host "What would you like to do?"
-    Write-Host "1) Mount WIM Image"
-    Write-Host "2) Unmount WIM Image"
+    Write-Host "What would you like to do?`n"
+    Write-Host "1) Mount ISO Image"
+    Write-Host "2) Unmount ISO Image"
+    Write-Host "3) Mount WIM Image"
+    Write-Host "4) Unmount WIM Image"
     Write-Host # Empty line for separation between the mounting and editing options
-    Write-Host "3) Find Indexes within ESD/WIM Image" # Index means an individual OS, such as Windows Pro
-    Write-Host "4) Convert ESD to WIM Image/Export Index of ESD/WIM Image"
-    Write-Host "5) Find Indexes within ESD/WIM Image"
-    Write-Host "6) Import Application Association XML File into WIM Image"
-    Write-Host "7) Build Custom ISO"
+    Write-Host "5) Find Indexes within ESD/WIM Image" # Index means an individual OS, such as Windows Pro
+    Write-Host "6) Convert ESD to WIM Image/Export Index of ESD/WIM Image"
+    Write-Host "7) Find Indexes within ESD/WIM Image"
+    Write-Host "8) Import Application Association XML File into WIM Image"
+    Write-Host "9) Build Custom ISO"
     Write-Host "$QUIT) Exit"
     Write-Host # Empty line for separation between menu and user input
 }
 
 
 function Perform-Choice([int]$userChoice) {
-    Clear-Host
-    Write-Host "We made it!"
+    switch ($choice) {
+        1 {
+            $pathToISO = Read-Host "Please specify a path to your ISO image"
+            $pathToISO = ($pathToISO -replace "`"", "") # Remove quotation marks in case the user adds them.
+            $pathToISO = $pathToISO.TrimEnd() # Remove ending spaces because they will conflict with using the variable later despite Windows still understanding the path
+            while ((-not (Test-Path $pathToISO)) -or (-not $pathToISO.EndsWith(".iso"))) {
+                $pathToISO = Read-Host "The ISO was not found. Please try again"
+                $pathToISO = ($pathToISO -replace "`"", "")
+                $pathToISO = $pathToISO.TrimEnd() # Remove ending spaces because they will conflict with using the variable later despite Windows still understanding the path
+            }
+            # Now that we know the path to the ISO, mount it:
+            Write-Host "`nMounting ISO...`n"
+            $mountResult = Mount-DiskImage -ImagePath "$pathToISO"
+            $mountDrive = ($mountResult | Get-Volume).DriveLetter + ":\" # We need to remember the drive letter so we can use it when modifying the WIM file.
+            $mountDriveFriendlyName = ($mountResult | Get-Volume).FileSystemLabel
+        }
+    }
+    #Clear-Host
+    Write-Host "Successfully mounted $pathToISO on $mountDrive ($mountDriveFriendlyName)"
     Write-Host # Empty line for separating function output from menu
 }
+
+
+
+
 
 Clear-Host
 # Loop the script until the user quits:
@@ -37,6 +61,4 @@ do {
             catch {} # Do nothing
     }
     Perform-Choice($choice)
-} until (
-    $choice -eq $QUIT
-)
+} until ($choice -eq $QUIT)
